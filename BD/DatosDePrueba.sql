cb7648bd93cd2856a4c7555ba7fde96315c837ea
mariomt <mariotinoco85@gmail.com>
2018-11-05 16:47:21 -0700

    Cambios en los procedimientos y se agregaron registros de prueba

--- BD/DatosDePrueba.sql
+++ BD/DatosDePrueba.sql
@@ -13,17 +13,22 @@ INSERT INTO tllaves(id,codigo,numero,id_aula) VALUES
 (null,44444444,9,2);
 */
 
-/*Registra una llave y el aula en caso de no existir*/
+/*Registra llave (y el aula en caso de no existir)*/
+
 CALL sp_registrar_llave(22222222,8,"5j","201");
 CALL sp_registrar_llave(33333333,9,"5j","202");
 CALL sp_registrar_llave(44444444,10,"5j","203");
 CALL sp_registrar_llave(55555555,11,"5j","204");
 
-
-
+/*Registrar maestro*/
+CALL sp_registrar_maestro(65654,"Jorge Romerin");
+CALL sp_registrar_maestro(65655,"bad navarro");
+CALL sp_registrar_maestro(65656,"alvaro suavecito");
 
 /*Registrar un horario*/
 call sp_registrar_horario(22222222,'2018','3',1515,"ESTRATEGIAS PARA APRENDER A APRENDER","LUNES,MARTES,MIERCOLES",'16:00:00','17:00:00');
 
 
 call sp_registrar_prestamo(null,"1,3,2");
+
+SELECT * from tusuarios;
