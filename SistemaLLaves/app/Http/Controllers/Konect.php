684eff574ce72f8d7c9ef0c3b1177ee469661106
xXAntonioXx <jose_antony11@hotmail.com>
2018-11-25 17:04:01 -0700

    cambiar charset

--- SistemaLLaves/app/Http/Controllers/Konect.php
+++ SistemaLLaves/app/Http/Controllers/Konect.php
@@ -4,7 +4,7 @@ function conectar(){
     $username="keySystem";
     $password="keySystem";
 
-    $cs = "mysql:host=localhost;dbname=sistema_llaves";
+    $cs = "mysql:host=localhost;dbname=sistema_llaves;charset=utf8";
     try{
         $conection = new PDO($cs,$username,$password);
         return $conection;
