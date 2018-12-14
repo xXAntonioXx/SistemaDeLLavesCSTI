3297931d6bf8fa19846e95cdec7394a8089f1918
xXAntonioXx <jose_antony11@hotmail.com>
2018-12-14 01:09:56 -0700

    deshabilitado la opcion de retroceso una vez se hace logout

--- SistemaLLaves/app/Http/Middleware/CheckSess.php
+++ SistemaLLaves/app/Http/Middleware/CheckSess.php
@@ -17,7 +17,10 @@ class CheckSess
     {   
 
         if($request->session()->get('estado')){
-            return $next($request);
+            return $next($request)->header('Cache-Control','nocache, no-store, max-age=0, must-revalidate')
+            ->header('Pragma','no-cache')
+            ->header('Expires','Sun, 02 Jan 1990 00:00:00 GMT');
+
         }
         return redirect('/');
     }
