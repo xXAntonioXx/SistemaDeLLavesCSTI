32f9a80983c3faf9231d2630c5a1cc78ee5f7a76
xXAntonioXx <jose_antony11@hotmail.com>
2019-01-09 01:03:07 -0700

    filtro de busqueda 60%

--- SistemaLLaves/app/Http/Controllers/BusquedaController.php
+++ SistemaLLaves/app/Http/Controllers/BusquedaController.php
@@ -29,21 +29,28 @@ class BusquedaController extends Controller
     }
 
     public function CargarRegistro(Request $req){
-        $timeMinimo = "{$req['fechaMinima']} {$req['horaMinima']}".":00";
-        $timeMaximo = "{$req['fechaMaxima']} {$req['horaMaxima']}".":00";
-        $maestro = $req['Maestro'];
-        $materia = $req['Materia'];
-        $area = mb_substr($req['Aula'],0,2);
-        $aula = mb_substr($req['Aula'],3,null);
+        $timeMinimo = "'{$req['fechaMinima']} {$req['horaMinima']}".":00'";
+        $timeMaximo = "'{$req['fechaMaxima']} {$req['horaMaxima']}".":00'";
 
-        $cadenaConsulta = "CALL sp_get_busqueda('{$timeMinimo}','{$timeMaximo}','{$maestro}','{$materia}','{$area}','{$aula}')";
-        /*$registros=$this->conexion->query($cadenaConsulta)->fetchAll();
+        $maestro = $req['Maestro'] ? "'{$req['Maestro']}'" : "NULL";
+        $materia = $req['Materia']?"'{$req['Materia']}'":"NULL";
+
+        $area="NULL";
+        $aula="NULL";
+        if(preg_match("/^5.*[kjgomKJGOM]{1}.*[-]{1}.*\d{3}/",$req['Aula'])){
+            $area = "'".mb_substr($req['Aula'],0,2)."'";
+            $aula = "'".mb_substr($req['Aula'],3,null)."'";
+        }
+
+        $cadenaConsulta = "CALL sp_get_busqueda({$timeMinimo},{$timeMaximo},{$maestro},{$materia},{$area},{$aula})";
+        $registros=$this->conexion->query($cadenaConsulta)->fetchAll();
         if($registros){
             return view('busqueda')->with('respuesta',json_encode($registros));
+            //return view('busqueda')->with('respuesta','respuesta');
         }else{
-            return view('busqueda');
-        }*/
-        return view('busqueda')->with('respuesta',$cadenaConsulta);
+            return view('busqueda')->with('respuesta',$cadenaConsulta);
+        }
+        //return view('busqueda')->with('respuesta',$cadenaConsulta);
 
     }
 }
