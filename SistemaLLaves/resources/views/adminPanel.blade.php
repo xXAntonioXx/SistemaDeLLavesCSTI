6e115aff2c120b5e8aaf641e958e8bd5ae2a9fd1
Mario Murillo <mariotinoco85@gmail.com>
2019-12-12 22:30:31 -0700

    Se le agregaron divs y clases a la vista adminPanel y de la misma forma se implementaron dichas clases en el css de la vista

--- SistemaLLaves/resources/views/adminPanel.blade.php
+++ SistemaLLaves/resources/views/adminPanel.blade.php
@@ -6,17 +6,29 @@
                 <h1>Bienvenido administrador</h1>
         </div>
         <div class="contenedor">
-            <div onclick="location.href='/panel/nuevoUsuario';">
-                <h2>Administrar Usuarios</h2>
-                <p>Agregar nuevos Usuarios o gestionar los que ya existen</p>
-           </div>
-            <div>
-                <h2>Cargar un Nuevo Horario</h2>
-                <p>Cambiar el horario de las clases en la base de datos a partir de un archivo CSV</p>
+            <div class="tarjeta" onclick="location.href='/panel/nuevoUsuario';">
+                <div class="encabezado-tarjeta">
+                    <h2>Administrar Usuarios</h2>
+                </div>
+                <div class="cuerpo-tarjeta">
+                    <p>Agregar nuevos Usuarios o gestionar los que ya existen</p>
+                </div>
             </div>
-            <div>
-                <h2>Cargar llaves de las Aulas</h2>
-                <p>Apartir de un archivo de csv se pueden cargar las llaves de las aulas</p>
+            <div class="tarjeta">
+                <div  class="encabezado-tarjeta">
+                    <h2>Cargar un Nuevo Horario</h2>
+                </div>
+                <div class="cuerpo-tarjeta">
+                    <p>Cambiar el horario de las clases en la base de datos a partir de un archivo CSV</p>
+                </div>
+            </div>
+            <div class="tarjeta">
+                <div class="encabezado-tarjeta">
+                    <h2>Cargar llaves de las Aulas</h2>
+                </div>
+                <div class="cuerpo-tarjeta">
+                    <p>Apartir de un archivo de csv se pueden cargar las llaves de las aulas</p>
+                </div>
             </div>
         </div>
     </div>
