08dea9c6dd29aa38bf51a10aaff051eba90a2a8e
Mario Murillo <mariotinoco85@gmail.com>
2019-12-13 22:36:38 -0700

    Se cambio el sp para actualizar al usuario

--- SistemaLLaves/utils/storedProcedures/UpdateUser.sql
+++ SistemaLLaves/utils/storedProcedures/UpdateUser.sql
@@ -7,14 +7,15 @@ BEGIN
 
     SELECT contrasena,rol INTO ContraNew,RolNew FROM tusuarios WHERE id = IdInput LIMIT 1;
 
-    IF(contraInput IS NOT NULL)THEN
-        SET ContraNew = contraInput;
-    END IF;
-    IF(rolInput IS NOT NULL)THEN
-        SET RolNew = rolInput;
-    END IF;
 
-    UPDATE tusuarios SET contrasena=ContraNew,rol=RolNew WHERE id = IdInput;
+    IF((contraInput IS NOT NULL AND LENGTH(contraInput)>0) AND rolInput IS NOT NULL)THEN
+        UPDATE tusuarios SET contrasena=contraInput,rol=rolInput WHERE id = IdInput;
+    ELSEIF((contraInput IS NULL OR LENGTH(contraInput)=0) AND rolInput IS NOT NULL)THEN
+        UPDATE tusuarios SET rol=rolInput WHERE id = IdInput;
+    ELSEIF((contraInput IS NOT NULL AND LENGTH(contraInput)>0) AND rolInput IS NULL)THEN
+        UPDATE tusuarios SET contrasena=contraInput WHERE id = IdInput;
+    END IF;
+    
 END //
 DELIMITER ; //
 call UpdateUser(1,'$2y$10$nqr.iOGwmeEpOe.46IPd0eBXa.amkUqLXgY64T9ekPl6z4/nxIsue',null);
