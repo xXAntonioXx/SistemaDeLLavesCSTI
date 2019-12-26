5620a7568f6af595c91eabcc0ed6b9e893559b06
xXAntonioXx <jose_antony11@hotmail.com>
2019-12-25 21:46:29 -0700

    funcionalidad terminada agregando diseño

--- SistemaLLaves/resources/views/actualizarHorarios.blade.php
+++ SistemaLLaves/resources/views/actualizarHorarios.blade.php
@@ -1,12 +1,15 @@
 @extends('layouts.navFoot')
 @section('cuerpo')
     <div style="text-align:center">
-        <form action="procesarArchivoHorarios" method="POST" enctype="multipart/form-data">
-            <label>Seleccione un archivo</label><br/>
+        <form action="procesarArchivoHorarios" method="POST" enctype="multipart/form-data" style="font-size:0.7em;text-align:left;margin-left:25px;">
+            <p >para actualizar el horario en el cual las aulas se estarán ocupando se requiere un archivo de excel csv <a href="/public/archivos/Horarios-Enero-2018.csv">ejemplo</a><br/>
+            NOTA:ya deben estar cargadas las llaves</p><br/><br/>
+            <label>Seleccione un archivo:</label>
             <input type="file" id="Horario_llaves" name="Horario_llaves" /><br/>
-            <input type="radio" name="ciclo" value="1"/>1<br/>
-            <input type="radio" name="ciclo" value="2"/>2<br/>
-            <input type="submit" id="mandar"/>
+            <label>Seleccione el ciclo escolar: </label>
+            <input type="radio" name="ciclo" value="1"/>Enero a Mayo
+            <input type="radio" name="ciclo" value="2"/>Agosto a Noviembre<br/>
+            <input type="submit" id="mandar" value="registrar horario" />
         {{csrf_field()}}
         </form>
     </div>
--- SistemaLLaves/resources/views/adminPanel.blade.php
+++ SistemaLLaves/resources/views/adminPanel.blade.php
@@ -6,13 +6,21 @@
                 <h1>Bienvenido administrador</h1>
         </div>
         <div class="contenedor">
-            <div onclick="location.href='/panel/nuevoUsuario';">
-                <h2>Administrar Usuarios</h2>
-                <p>Agregar nuevos Usuarios o gestionar los que ya existen</p>
+            <div class="tarjeta" onclick="location.href='/panel/nuevoUsuario';">
+                <div  class="encabezado-tarjeta">
+                    <h2>Administrar Usuarios</h2>
+                </div>
+                <div class="cuerpo-tarjeta">
+                    <p>Agregar nuevos Usuarios o gestionar los que ya existen</p>
+                </div>
            </div>
-            <div onclick="location.href='/panel/actualizarHorarios';">
-                <h2>Cargar un Nuevo Horario</h2>
-                <p>Cambiar el horario de las clases en la base de datos a partir de un archivo CSV</p>
+            <div class="tarjeta" onclick="location.href='/panel/actualizarHorarios';">
+                <div  class="encabezado-tarjeta">
+                    <h2>Cargar un Nuevo Horario</h2>
+                </div>
+                <div class="cuerpo-tarjeta">
+                    <p>Cambiar el horario de las clases en la base de datos a partir de un archivo CSV</p>
+                </div>
             </div>
             <div class="tarjeta">
                 <div  class="encabezado-tarjeta">
